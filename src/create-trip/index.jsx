88ff34f68198c1bb6bd84af66387ac982b25cb9a
Bhargavi <bhargavi_karuturi@srmap.edu.in>
2025-03-26 15:31:09 +0530

    modified

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function CreateTrip() {
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
         <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
         <p>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+        <div>
+          <div className='mt-20'>
+            <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
+            {/* <GooglePlacesAutocomplete
+               apikey=''
+            /> */}
+          </div>
+        </div> 
     </div>
   )
 }
