56bb5d311d7f4dce6cdebc524315c3371fef56c8
Bhargavi <bhargavi_karuturi@srmap.edu.in>
2025-03-26 14:47:10 +0530

    Landing page created

---
+++ src/create-trip/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CreateTrip() {
+  return (
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
+        <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+        <p>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+    </div>
+  )
+}
+
+export default CreateTrip
